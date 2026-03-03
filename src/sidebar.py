dd7d0abd4100c5b914b8294ab1a4dda1864d3914
alecmstockman <alecmstockman@gmail.com>
2026-03-02 19:00:56 -0600

    sidebar library and playlist now locked

--- src/sidebar.py
+++ src/sidebar.py
@@ -13,6 +13,8 @@ class Sidebar(ttk.Frame):
         self.playlist = playlist
         self.playlist_manager = playlist_manager
         self.selected_view = None
+        self.library_id = None
+
         self.playlist_id = None
         self.selected_user_playlist = None
 
@@ -25,22 +27,31 @@ class Sidebar(ttk.Frame):
         self.sidebar_tree.bind("<<TreeviewSelect>>", self.on_sidebar_click)
         self.sidebar_tree.bind("<Button-2>", self.on_right_click)
         self.sidebar_tree.bind("<Control-Button-1>", self.on_right_click)
+        
+        self.sidebar_tree.bind("<<TreeviewOpen>>", self.lock_parents)
+        self.sidebar_tree.bind("<<TreeviewClose>>", self.lock_parents)
+        
 
 
     def set_sidebar(self):
-        library_id = self.sidebar_tree.insert("", "end", text="Library", values=("Library"), open=True)
+        self.library_id = self.sidebar_tree.insert("", "end", text="Library", values=("Library"), open=True)
         self.playlist_id = self.sidebar_tree.insert("", "end", text="Playlists", values=("Playlists"))
+
+        self.locked_parents = [self.library_id, self.playlist_id]
+        for iid in self.locked_parents:
+            self.sidebar_tree.item(iid, open=True)
+
         
-        self.sidebar_tree.insert(library_id, "end", text="Artists", values=("Artists", ))
-        self.sidebar_tree.insert(library_id, "end", text="Albums", values=("Albums", ))
-        self.sidebar_tree.insert(library_id, "end", text="Songs", values=("Songs", ))
-        self.sidebar_tree.insert(library_id, "end", text="Favorites", values=("Favorites", ))
+        self.sidebar_tree.insert(self.library_id, "end", text="Artists", values=("Artists", ))
+        self.sidebar_tree.insert(self.library_id, "end", text="Albums", values=("Albums", ))
+        self.sidebar_tree.insert(self.library_id, "end", text="Songs", values=("Songs", ))
+        self.sidebar_tree.insert(self.library_id, "end", text="Favorites", values=("Favorites", ))
 
         self.sidebar_tree.insert(self.playlist_id, "end", text="All Playlists", values=("All Playlists"))
         
         self.popup_menu.add_command(label="Delete Playlist", command=self.delete_user_playlist)
 
-        self.sidebar_tree.item(library_id, open=True)
+        self.sidebar_tree.item(self.library_id, open=True)
         self.sidebar_tree.item(self.playlist_id, open=True)
 
         self.set_user_playlists()
@@ -54,6 +65,18 @@ class Sidebar(ttk.Frame):
             self.sidebar_tree.insert(self.playlist_id, "end", iid=value.id, text=f"- {value.name}", values=(value.id, value.name))
         
         children = self.sidebar_tree.get_children(self.playlist_id)
+
+    def lock_parents(self, event=None):
+        for iid in self.locked_parents:
+            if iid in self.sidebar_tree.get_children():
+                self.sidebar_tree.item(iid, open=True)
+
+    def on_sidebar_key(self, event):
+        if event.keysym == "space":
+            selection = self.sidebar_tree.selection()
+            for iid in selection:
+                if iid in self.locked_parents:
+                    return "break"
 
     def on_sidebar_click(self, event):
         selection = self.sidebar_tree.selection()
